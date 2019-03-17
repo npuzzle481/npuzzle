14cf2cfef18daabdcb8cdb981e57131a9b1f1d40
Atakan Gucsav <atakangucsav@gmail.com>
2019-03-17 22:57:17 +0300

    successor object class and header files

---
+++ succobject.h
@@ -0,0 +1,14 @@
+#ifndef SUCCOBJECT_H
+#define SUCCOBJECT_H
+
+
+class succObject{
+public:
+    succObject(int row2, int col2, int rowold2, int colold2);
+    int row;
+    int col;
+    int rowold;
+    int colold;
+};
+
+#endif // SUCCOBJECT_h
