244d8174703ab13057868aabf4aacf7cc520c86f
ozgenil <46612470+ozgenil@users.noreply.github.com>
2019-03-18 22:53:23 +0300

    Commit state.cpp - State of matrix.

---
+++ Npuzzle_SourceCode/state.cpp
@@ -0,0 +1,281 @@
+#include "state.h"
+#include <iostream>
+#include <string>
+#include "QString"
+//states
+using namespace std;
+State::State() {
+
+}
+void State::addCost(int a) {}
+
+void State::update(Matrix elementMatrix) {}
+
+State::State(Matrix elementMatrix){}
+
+QString State::showMatrix(){}
+
+Matrix State::moveTile(int row, int col, int rowold, int colold) {}
+
+/*
+ *Look for position of 0, and determine type accordingly
+ * */
+void State::determineType(){
+    uniqueID="";
+    int zeronumber=0;
+
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
+            uniqueID.append(QString::number(StateMatrix[i][j]));
+
+            if(StateMatrix[i][j]==0) {
+                indexCol[zeronumber]=j;
+                indexRow[zeronumber]=i;
+                // Zero is found, determine where it is
+                if(i==0){
+                    if(j==0){
+                        if (StateMatrix[i+1][j]==0) {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        else if (StateMatrix[i][j+1]==0) {
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        else  {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        //type=1;
+                    }
+                    else if (j==(n-1)) {
+                        if (StateMatrix[i][j-1]==0) {
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        else if (StateMatrix[i+1][j]==0) {
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        else  {
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        //type=2;
+                    }
+                    else {
+                        if (StateMatrix[i][j-1]==0) {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        else if (StateMatrix[i][j+1]==0) {
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        else if (StateMatrix[i+1][j]==0) {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                             // LEFT
+                             succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                        else  {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                         }
+                        //type=5;
+                    }
+
+                }
+                else if (i==(n-1))
+                    if(j==0){
+                        if (StateMatrix[i-1][j]==0) {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                       else if (StateMatrix[i][j+1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            }
+                        else  {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        //type=4;
+                            }
+                    else if (j==(n-1)) {
+                        if (StateMatrix[i-1][j]==0) {
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                       else if (StateMatrix[i][j-1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            }
+                        else  {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        //type=3;
+                            }
+                    else {
+                        if (StateMatrix[i][j-1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                       else if (StateMatrix[i][j+1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                            }
+                        else if (StateMatrix[i-1][j]==0) {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                             // LEFT
+                             succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        else  {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        //type=7;
+                    }
+
+                else {
+                    if(j==0){
+                        if (StateMatrix[i-1][j]==0) {
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                       else if (StateMatrix[i][j+1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            }
+                        else if (StateMatrix[i+1][j]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));;
+                             }
+                        else  {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        //type=8;
+                            }
+                    else if (j==(n-1)) {
+                        if (StateMatrix[i-1][j]==0) {
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                       else if (StateMatrix[i][j-1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            }
+                        else if (StateMatrix[i+1][j]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        else  {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        //type=6;
+                            }
+                    else {
+                        if (StateMatrix[i-1][j]==0) {
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                        }
+                       else if (StateMatrix[i][j+1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                            }
+                        else if (StateMatrix[i+1][j]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        else if (StateMatrix[i][j-1]==0) {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        else  {
+                            // UP
+                            succsList.append(succObject(indexRow[zeronumber]-1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // RIGHT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]+1,indexRow[zeronumber],indexCol[zeronumber]));
+                            // DOWN
+                            succsList.append(succObject(indexRow[zeronumber]+1,indexCol[zeronumber],indexRow[zeronumber],indexCol[zeronumber]));
+                            // LEFT
+                            succsList.append(succObject(indexRow[zeronumber],indexCol[zeronumber]-1,indexRow[zeronumber],indexCol[zeronumber]));
+                             }
+                        //type=0;
+                    }
+                }
+                
+                zeronumber++;
+            }
+
+        }
+    }     
+}
+
+
+
+
