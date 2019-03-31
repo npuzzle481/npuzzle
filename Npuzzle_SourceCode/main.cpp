ddcfb8ef741cc078374e47a1d90ebbc4222d18b3
ozgenil <46612470+ozgenil@users.noreply.github.com>
2019-03-31 11:05:21 +0300

    Update main.cpp

--- Npuzzle_SourceCode/main.cpp
+++ Npuzzle_SourceCode/main.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "globals.h"
 #include <time.h>
+#include <state.h>
+#include <pq.h>
+#include <statelist.h>
 
 
+//init
+State* initialState=new State();
+State* finalState=new State();
 
+
+bool* isARunning=new bool(0);
+bool* isIterativeRunning=new bool(0);
+
+QList<stateList> *A_visited=new QList<stateList>();
+QList<stateList> *back2initial=new QList<stateList>();
+
+PQ* A_PQ=new PQ();
+
+QString *A_finalID= new QString("");
+
+int * iteration_number = new int(0);
+int * max_iteration=new int(0);
+bool * iteration_step = new bool(0);
 
 int main(int argc, char *argv[])
 {
-
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
