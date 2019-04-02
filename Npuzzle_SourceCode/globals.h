11318fd9b706671251c75aa44ec93f0884f4198f
ZeynepIrem <45374134+ZeynepIrem@users.noreply.github.com>
2019-04-02 21:16:14 +0300

    Update dfs globals

--- Npuzzle_SourceCode/globals.h
+++ Npuzzle_SourceCode/globals.h
@@ -15,7 +15,14 @@ extern bool* isBFSRunning;
 extern QString *BFS_finalID;
 extern int* iteration_number;
 extern bool * iteration_step;
-
+extern State* initialState;
+extern State* finalState;
+extern QList<stateList> *DFS_visited;
+extern PQ *A_PQ;
+extern bool* isDFSRunning;
+extern QString *DFS_finalID;
+extern int* dfs_iteration_number;
+extern QList<stateList> *DFS_Stack;
 #endif // GLOBAL
 
 
