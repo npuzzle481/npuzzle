894e52f954bee8ba7718c237373c1763d928d89f
ozgenil <46612470+ozgenil@users.noreply.github.com>
2019-04-02 23:26:15 +0300

    update globals.h

--- Npuzzle_SourceCode/globals.h
+++ Npuzzle_SourceCode/globals.h
@@ -1,29 +1,42 @@
 #ifndef GLOBALS
 #define GLOBALS
+#include <QApplication>
+#include <QtCore/QCoreApplication>
 #include <QString>
+#include <QFile>
+#include <QDebug>
+#include <QTextStream>
+#include <QList>
 #include <state.h>
+#include <QTime>
 #include "statelist.h"
 #include "succobject.h"
+#include <QMessageBox>
+#include <QStack>
 #include "pq.h"
 #include "pqitem.h"
 
-extern State* BFS_currentState;
-extern QList<stateList> *BFS_visited;
-extern QList<stateList>  *BFS_nextStates;
-extern QList<stateList>  *BFS_currentStates;
-extern bool* isBFSRunning;
-extern QString *BFS_finalID;
-extern int* iteration_number;
-extern bool * iteration_step;
 extern State* initialState;
 extern State* finalState;
+extern State* BFS_currentState;
+extern QList<stateList> *BFS_visited;
 extern QList<stateList> *DFS_visited;
-extern PQ *A_PQ;
-extern bool* isDFSRunning;
-extern QString *DFS_finalID;
-extern int* dfs_iteration_number;
+extern QList<stateList> *A_visited;
+extern QList<stateList>  *BFS_nextStates;
+extern QList<stateList>  *BFS_currentStates;
+extern QList<stateList> *back2initial;
 extern QList<stateList> *DFS_Stack;
+extern PQ *A_PQ;
+extern QString *BFS_finalID;
+extern QString *A_finalID;
+extern QString *DFS_finalID;
+extern bool* isBFSRunning;
+extern bool* isDFSRunning;
+extern bool* isARunning;
+extern bool* isIterativeRunning;
+extern int* iteration_number;
+extern int* dfs_iteration_number;
+extern int* max_iteration;
+extern bool * iteration_step;
+
 #endif // GLOBAL
-
-
-
